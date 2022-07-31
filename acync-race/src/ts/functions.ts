f2edbda9254df747fa069606dfca9f166de6600c
Legat <zia-d@mail.ru>
2022-07-31 17:09:31 +0400

    feat: implement functions for random cars generation and deletion one and all cars from garage

--- acync-race/src/ts/functions.ts
+++ acync-race/src/ts/functions.ts
@@ -1,4 +1,35 @@
+import { Brands, Models } from './types';
+
 export function generateRandomNumber(min: number, max: number) {
   const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
   return randomNumber;
+}
+
+export function generateRandomName(): string {
+  const quantityOfBrands: number = Object.keys(Brands).length;
+  const numberOfBrand: number = generateRandomNumber(0, quantityOfBrands - 1);
+  const carBrand: string = Object.keys(Brands)[numberOfBrand];
+  // TODO: Использую Enum как обычный объект. Если будет время,
+  // узнать, как получить название свойства энума, зная его значение и поменять.
+  console.log('carBrand: ', carBrand);
+
+  const quantityOfModels: number = Object.keys(Models).length;
+  const numberOfModels: number = generateRandomNumber(0, quantityOfModels - 1);
+  const carModel: string = Object.keys(Models)[numberOfModels];
+  console.log('carModel: ', carModel);
+
+  const carName = `${carBrand} ${carModel}`;
+  return carName;
+}
+
+export function generateRandomColor(): string {
+  const red: number = generateRandomNumber(0, 255);
+  const red16: string = (`0${red.toString(16)}`).slice(-2);
+  const green: number = generateRandomNumber(0, 255);
+  const green16: string = (`0${green.toString(16)}`).slice(-2);
+  const blue: number = generateRandomNumber(0, 255);
+  const blue16: string = (`0${blue.toString(16)}`).slice(-2);
+  const carColor = `#${red16}${green16}${blue16}`;
+  console.log('Car color: ', carColor);
+  return carColor;
 }
--- acync-race/src/ts/model/mock-data.ts
+++ acync-race/src/ts/model/mock-data.ts
@@ -1,18 +1,20 @@
-import { generateRandomNumber } from '../functions';
+import { generateRandomColor, generateRandomName } from '../functions';
 import { Car, Requests } from '../types';
 
 export class Data {
   serverUrl = 'http://127.0.0.1:3000';
+
   garageRes = '/garage';
+
   winnersRes = '/winners';
+
   garageUrl = `${this.serverUrl}${this.garageRes}`;
 
-  carCreateHeader = {'Content-Type': 'application/json'};
+  carCreateHeader = { 'Content-Type': 'application/json' };
 
   async getCars(): Promise<Car[]> {
     const response: Response = await fetch(this.garageUrl, { method: Requests.GET });
     const data: Car[] = await response.json();
-    console.log('inside method: ', data);
     return data;
   }
 
@@ -20,27 +22,44 @@ export class Data {
     const carUrl = `${this.garageUrl}/${carId}`;
     const response: Response = await fetch(carUrl, { method: Requests.GET });
     const data: Car = await response.json();
-    console.log('carUrl: ', carUrl, 'carId: ', carId);
-    console.log('inside method: ', data);
     return data;
   }
 
   createCar(name: string, color: string): void { // TODO: Добавить обработку ответа сервера
-    const newCar: Car = {name: name, color: color};
-    fetch(this.garageUrl, { method: Requests.POST, headers: this.carCreateHeader, body: JSON.stringify(newCar)});
+    const newCar: Car = { name, color };
+    fetch(this.garageUrl, { method: Requests.POST, headers: this.carCreateHeader,
+      body: JSON.stringify(newCar) });
     console.log('newCar: ', newCar);
+  }
+
+  createRandomCars(newCarsQuantity: number) {
+    for (let i = 0; i < newCarsQuantity; i += 1) {
+      this.createCar(generateRandomName(), generateRandomColor());
+    }
+  }
+
+  deleteCar(carId: number): void { // TODO: Добавить обработку ответа сервера
+    const carUrl = `${this.garageUrl}/${carId}`;
+    fetch(carUrl, { method: Requests.DELETE });
+    console.log(`Car ${carId} deleted`);
+  }
+
+  async deleteAllCars() {
+    const allCarsInGarage: Car[] = await this.getCars();
+    allCarsInGarage.forEach((car): void => {
+      this.deleteCar(car.id as number);
+    });
   }
 }
 
-const dataGetter = new Data(); // TODO: Это было добавлено для проверки. Теория подтверждается. Нужно получать данные внутри функций.
+const dataGetter = new Data(); // TODO: Это было добавлено для проверки.
+                    //Теория подтверждается. Нужно получать данные внутри функций.
 
 async function getData() {
   const cars = await dataGetter.getCars();
-  console.log('in function: ', cars);
   const car = await dataGetter.getCar(2);
-  console.log('in function: ', car);
 }
 
 getData();
-dataGetter.createCar('SuperCar', 'red');
-dataGetter.createCar('SuperCar2', 'blue');
+
+dataGetter.deleteAllCars();
